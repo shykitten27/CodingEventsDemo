2da7c39f0e1458c3ac59893cac52d4f2948b96e4
Donna Seidel <dseidel01@gmail.com>
2021-09-14 18:59:57 -0500

    apply validation attributes

--- CodingEventsDemo/ViewModels/AddEventViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CodingEventsDemo.ViewModels
 {
     public class AddEventViewModel
     {
+        [Required(ErrorMessage ="Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")] //min 3 and max 50
         public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter a description for your event.")]
+        [StringLength(500, ErrorMessage = "Description is too long!")] //max 500
         public string Description { get; set; }
+        [EmailAddress] //helps validate
+        public string ContactEmail { get; set; }
     }
 }
