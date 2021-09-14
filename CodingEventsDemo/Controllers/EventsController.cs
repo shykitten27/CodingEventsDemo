a44c041372b5d7d39128ef2bf83194cdb69e73ed
Donna Seidel <dseidel01@gmail.com>
2021-09-14 18:44:52 -0500

    add a viewmodel

--- CodingEventsDemo/Controllers/EventsController.cs
+++ CodingEventsDemo/Controllers/EventsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEventsDemo.Data;
 using CodingEventsDemo.Models;
+using CodingEventsDemo.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -17,19 +18,26 @@ namespace coding_events_practice.Controllers
         public IActionResult Index()
         {
             List<Event> events = new List<Event>(EventData.GetAll());
+            //pass list of event objects to the view
 
             return View(events);
         }
 
-        public IActionResult Add()
+        public IActionResult Add() //retrieve view form
         {
-            return View();
+            AddEventViewModel addEventViewModel = new AddEventViewModel(); //create instance of obj type
+            return View(addEventViewModel); //then pass it into the view
         }
 
         [HttpPost]
-        [Route("Events/Add")]
-        public IActionResult NewEvent(Event newEvent)
+        public IActionResult Add(AddEventViewModel addEventViewModel) //process view form
         {
+            Event newEvent = new Event
+            {
+                Name = addEventViewModel.Name,
+                Description = addEventViewModel.Description
+            };
+
             EventData.Add(newEvent);
       
             return Redirect("/Events");
