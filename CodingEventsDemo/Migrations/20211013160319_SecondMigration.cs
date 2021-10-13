35f4efd03c1619b52bdaa4c2e55922b79c9fc23b
Donna Seidel <dseidel01@gmail.com>
2021-10-13 11:59:27 -0500

    dev

---
+++ CodingEventsDemo/Migrations/20211013160319_SecondMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CodingEventsDemo.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EventCategories",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EventCategories", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EventCategories");
+        }
+    }
+}
---
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodingEventsDemo.Models
+{
+    public class EventCategory
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public EventCategory()
+        {
+        }
+
+        public EventCategory(string name)
+        {
+            Name = name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is EventCategory @eventCategory &&
+                   Id == @eventCategory.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
