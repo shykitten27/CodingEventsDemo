e3c645661e51445cc9fc3e90fa67a5e9123300fc
Donna Seidel <dseidel01@gmail.com>
2021-10-14 15:16:04 -0500

    create one-to-many relationship replace EventType with EventCategory Define inverse relationship refactor controller and view

--- CodingEventsDemo/ViewModels/AddEventViewModel.cs
+++ CodingEventsDemo/ViewModels/AddEventViewModel.cs
@@ -12,22 +12,35 @@ namespace CodingEventsDemo.ViewModels
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Description is required")]
+        [Required(ErrorMessage = "Description is required.")]
         [StringLength(500, ErrorMessage = "Description too long!")]
         public string Description { get; set; }
 
         [EmailAddress]
         public string ContactEmail { get; set; }
 
-        public EventType Type { get; set; }
+        [Required(ErrorMessage = "Category is required.")]
+        public int CategoryId { get; set; }
 
-        public List<SelectListItem> EventTypes { get; set;  } = new List<SelectListItem>
+        public List<SelectListItem> Categories { get; set; }
+
+        public AddEventViewModel(List<EventCategory> categories)
         {
-            new SelectListItem(EventType.Conference.ToString(), ((int)EventType.Conference).ToString()),
-            new SelectListItem(EventType.Meetup.ToString(), ((int)EventType.Meetup).ToString()),
-            new SelectListItem(EventType.Social.ToString(), ((int)EventType.Social).ToString()),
-            new SelectListItem(EventType.Workshop.ToString(), ((int)EventType.Workshop).ToString())
-        };
+            Categories = new List<SelectListItem>();
 
+            foreach (var category in categories)
+            {
+                Categories.Add(
+                    new SelectListItem
+                    {
+                        Value = category.Id.ToString(),
+                        Text = category.Name
+                    }
+                 ); 
+            }
+        }
+        public AddEventViewModel() 
+        { 
+        }
     }
 }
